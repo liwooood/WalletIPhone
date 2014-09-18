a76c1cab6de395352f8e1f757a49794104b4b04d
chenhf <chenhf@chenhfdeMac-mini.local>
2014-09-18 21:47:13 +0800

    Initial Commit

---
+++ WalletIPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WalletIPhone
+//
+//  Created by chenhf on 14-9-18.
+//  Copyright (c) 2014年 product. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WalletIPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WalletIPhone
+//
+//  Created by chenhf on 14-9-18.
+//  Copyright (c) 2014年 product. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
